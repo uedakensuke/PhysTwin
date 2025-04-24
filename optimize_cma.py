f7750ff391cf0f36ff03de67116a41fb6fc10ad1
uedakensuke <ueda.kensuke@robocip.or.jp>
2025-04-24 16:41:26 +0900

    refactor

--- optimize_cma.py
+++ optimize_cma.py
@@ -1,15 +1,18 @@
 # The first stage to optimize the sparse parameters using CMA-ES
+import os
+import random
+import sys
+import json
+from argparse import ArgumentParser
+
 from qqtt import OptimizerCMA
 from qqtt.utils import logger, cfg
 from qqtt.utils.logger import StreamToLogger, logging
-import os
-import random
 import numpy as np
-import sys
 import torch
-import pickle
-import json
-from argparse import ArgumentParser
+
+from .data_process.utils.path import PathResolver
+from .data_process.utils.data_reader import CameraInfo
 
 DIR = os.path.dirname(__file__)
 
@@ -39,11 +42,15 @@ sys.stderr = StreamToLogger(logger, logging.ERROR)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
+    parser.add_argument("--raw_path", type=str, default=None)
     parser.add_argument("--base_path", type=str, required=True)
     parser.add_argument("--physics_sparse_path", type=str, required=True)
     parser.add_argument("--case_name", type=str, required=True)
     parser.add_argument("--max_iter", type=int, default=20)
     args = parser.parse_args()
+
+    if args.raw_path is None:
+        args.raw_path = args.base_path
 
     base_path = args.base_path
     physics_sparse_path = args.physics_sparse_path
@@ -59,20 +66,18 @@ if __name__ == "__main__":
     base_dir = f"{physics_sparse_path}/{case_name}"
 
     # Set the intrinsic and extrinsic parameters for visualization
-    with open(f"{base_path}/{case_name}/calibrate.pkl", "rb") as f:
-        c2ws = pickle.load(f)
-    w2cs = [np.linalg.inv(c2w) for c2w in c2ws]
-    cfg.c2ws = np.array(c2ws)
-    cfg.w2cs = np.array(w2cs)
-    with open(f"{base_path}/{case_name}/metadata.json", "r") as f:
-        data = json.load(f)
-    cfg.intrinsics = np.array(data["intrinsics"])
-    cfg.WH = data["WH"]
-    cfg.overlay_path = f"{base_path}/{case_name}/color"
+    path_resolver = PathResolver(args.raw_path, args.base_path, args.case_name)
+    camera_info = CameraInfo(path_resolver)
+    cfg.c2ws = camera_info.c2ws
+    cfg.w2cs = camera_info.w2cs
+    cfg.intrinsics = camera_info.intrinsics
+    cfg.WH = camera_info.WH
+    cfg.overlay_path = path_resolver.get_color_dir()
+
 
     logger.set_log_file(path=base_dir, name="optimize_cma_log")
     optimizer = OptimizerCMA(
-        data_path=f"{base_path}/{case_name}/final_data.pkl",
+        data_path=path_resolver.final_data_pkl,
         base_dir=base_dir,
         train_frame=train_frame,
     )
